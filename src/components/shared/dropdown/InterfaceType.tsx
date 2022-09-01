0f656f28ba377c0c4a71d5115bcfa5c3785e324e
Message Akunna <message.ao@gmail.com>
2022-09-01 14:16:57 +0100

    Resource and app dropdown completed

--- src/components/shared/dropdown/InterfaceType.tsx
+++
@@ -1,9 +0,0 @@
-export interface NavbarMenuInt {
-  children: JSX.Element;
-  title: string;
-  popupId?: string;
-}
-
-export declare type ImageButtonFunc = () => JSX.Element;
-// eslint-disable-next-line no-unused-vars
-export declare type NavbarMenuFunc = (props: NavbarMenuInt) => JSX.Element;
