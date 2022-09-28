3581dccc60d73b5ad73e4aa471cfb52a37904206
Moses Peter <mohemos@gmail.com>
2022-09-28 02:37:57 +0100

    update

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-import { devLog } from "./utils";
-
-export async function middleware(req: NextRequest) {
-  //   devLog("cookies token", req.cookies.get("token"));
-}
