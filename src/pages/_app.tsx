72462f40e32845e949a7c55e6f411a2ca566b9be
Message Akunna <message.ao@gmail.com>
2022-09-30 05:55:31 +0100

    Page loader added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,10 @@
+// React component
+import { useEffect, useState } from "react";
+// next components
+import Router from "next/router";
+// mui component
+import LinearProgress from "@mui/material/LinearProgress";
+//
 import type { AppProps } from "next/app";
 import { Hydrate, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -13,16 +20,30 @@ import Custom404 from "./404";
 import "@src/styles/pdfReader.css";
 
 function App({ Component, pageProps }: AppProps) {
+  const [pageLoading, setPageLoading] = useState(false);
   const client = useRef(queryClient);
   const { cachedData = null } = pageProps as BasePageProps;
 
   queryClient.setQueryData("pageProps", pageProps);
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", () => {
+      setPageLoading(true);
+    });
+    Router.events.on("routeChangeComplete", () => {
+      setPageLoading(false);
+    });
+    Router.events.on("routeChangeError", () => {
+      setPageLoading(false);
+    });
+  }, []);
 
   return (
     <QueryClientProvider client={client.current}>
       <Hydrate state={pageProps.dehydrateState}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          {pageLoading && <LinearProgress sx={{ zIndex: 2000 }} />}
           {cachedData?.centre ? <Component {...pageProps} /> : <Custom404 />}
         </ThemeProvider>
       </Hydrate>
