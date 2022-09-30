d28b55a6262ec11a196f1cbe2a97477d69de0d89
Message Akunna <message.ao@gmail.com>
2022-09-30 16:13:23 +0100

    Removed contentionary home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 //
 import { theme } from "@src/styles/theme";
+import { useTheme } from "@mui/material/styles";
 import { useRef } from "react";
 import { queryClient } from "@src/utils";
 import { BasePageProps } from "@src/utils/interface";
@@ -20,22 +21,23 @@ import Custom404 from "./404";
 import "@src/styles/pdfReader.css";
 
 function App({ Component, pageProps }: AppProps) {
+  const appTheme = useTheme();
   const [pageLoading, setPageLoading] = useState(false);
+  //
   const client = useRef(queryClient);
   const { cachedData = null } = pageProps as BasePageProps;
 
   queryClient.setQueryData("pageProps", pageProps);
 
   useEffect(() => {
-    Router.events.on("routeChangeStart", () => {
-      setPageLoading(true);
-    });
-    Router.events.on("routeChangeComplete", () => {
-      setPageLoading(false);
-    });
-    Router.events.on("routeChangeError", () => {
-      setPageLoading(false);
-    });
+    Router.events.on("routeChangeStart", () => setPageLoading(true));
+    Router.events.on("routeChangeComplete", () => setPageLoading(false));
+    Router.events.on("routeChangeError", () => setPageLoading(false));
+    return () => {
+      Router.events.off("routeChangeStart", () => setPageLoading(true));
+      Router.events.off("routeChangeComplete", () => setPageLoading(false));
+      Router.events.off("routeChangeError", () => setPageLoading(false));
+    };
   }, []);
 
   return (
@@ -43,7 +45,15 @@ function App({ Component, pageProps }: AppProps) {
       <Hydrate state={pageProps.dehydrateState}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          {pageLoading && <LinearProgress sx={{ zIndex: 2000 }} />}
+          {pageLoading && (
+            <LinearProgress
+              sx={{
+                top: 0,
+                position: "fixed",
+                zIndex: appTheme.zIndex.drawer + 4,
+              }}
+            />
+          )}
           {cachedData?.centre ? <Component {...pageProps} /> : <Custom404 />}
         </ThemeProvider>
       </Hydrate>
