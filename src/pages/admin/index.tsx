1520a2cdd38c66dfabc9137b431d037743a556b4
Deborah <career.debbie@gmail.com>
2022-10-05 15:27:01 +0100

    update

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,16 +1,11 @@
 import AdminPage from "@src/components/manageDomain/dashboard";
-import Wrapper from "@src/components/manageDomain";
 import { getAuthData } from "@src/utils/auth";
 import { getCentre, handleError } from "@src/utils";
 import { CachedCentreInt } from "@src/utils/interface";
 import { GetServerSideProps } from "next";
 
 const AdminPageEntry = () => {
-  return (
-    <Wrapper>
-      <AdminPage />
-    </Wrapper>
-  );
+  return <AdminPage />;
 };
 
 export default AdminPageEntry;
