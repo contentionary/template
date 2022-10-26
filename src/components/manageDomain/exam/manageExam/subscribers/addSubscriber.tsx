397c4fee0724b8e4e1f63c9e58f91b18080d45b1
Moses Peter <mohemos@gmail.com>
2022-10-26 12:01:26 +0100

    Integreted exam manager

--- src/components/manageDomain/exam/manageExam/subscribers/addSubscriber.tsx
+++ src/components/manageDomain/exam/manageExam/subscribers/addSubscriber.tsx
@@ -45,7 +45,7 @@ const AddSubscriber = ({
         onClick={() =>
           exam.isPrivate
             ? openDialog()
-            : toggleToast("This is only avaible private")
+            : toggleToast("This is only available for private centres")
         }
         sx={{ fontSize: 18 }}
       >
