d3a28f970913bcabe5dd8d5b734d72560fb38e86
Deborah <career.debbie@gmail.com>
2022-12-08 11:38:37 +0000

    update

--- src/pages/admin/exam/[id]/manage-exam.tsx
+++ src/pages/admin/exam/[id]/manage-exam.tsx
@@ -34,6 +34,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       },
     };
   } catch (error) {
+    console.log(error)
     return { props: { error: handleError(error) } };
   }
 };
